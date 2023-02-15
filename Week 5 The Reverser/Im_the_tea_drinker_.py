8218f9f8cee4bc8a3fe82865b197a66790fad592
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-02-15 21:09:42 +0000

    Week 5 and 6

---
+++ Week 5 The Reverser/Im_the_tea_drinker_.py
@@ -0,0 +1,8 @@
+
+def backwards(correct):
+    x = correct[::-1]
+    done = x.swapcase()
+    return done
+ 
+print(backwards("tEst"))
+
---
+++ Week 6 FIZZBUZZ/Im_the_tea_drinker.py
@@ -0,0 +1,15 @@
+x = int(input("Enter a number: "))
+
+def fizzbuzz(x):
+    if x % 3 == 0 and x % 5 == 0:
+        return "FizzBuzz"
+    elif x % 3 == 0:
+        return "Fizz"
+    elif x % 5 == 0:
+        return "Buzz"
+    else:
+        return x
+
+
+
+print(fizzbuzz(x))
